c30776ec304418792650b128ad54fff190edf417
Crane-tsuru <116482604+Crane-tsuru@users.noreply.github.com>
2024-05-01 21:35:38 +0900

    first commit

---
+++ yumemicoding/yumemicodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  yumemicodingApp.swift
+//  yumemicoding
+//
+//  Created by 鶴見駿 on 2024/05/01.
+//
+
+import SwiftUI
+
+@main
+struct yumemicodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
