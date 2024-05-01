52ae811b1670a28b7bb939b353ec9390da1f8028
Crane-tsuru <116482604+Crane-tsuru@users.noreply.github.com>
2024-05-01 23:46:10 +0900

    add Button for fortune

--- yumemicoding/ContentView.swift
+++ yumemicoding/ContentView.swift
@@ -10,15 +10,40 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//            NavigationLink(destination: ExampleView()) {
+//                Rectangle()
+//                    .frame(width: 200, height: 100)
+//                    .foregroundStyle(.purple.gradient)
+//                    .cornerRadius(10)
+//                    .overlay(Text("占う").font(.title).foregroundStyle(.white))
+//            }
+            
+            NavigationLink {
+                ExampleView()
+            } label: {
+                Rectangle()
+                    .frame(width: 200, height: 100)
+                    .foregroundStyle(.purple.gradient)
+                    .cornerRadius(10)
+                    .overlay(Text("占う").font(.title).foregroundStyle(.white))
+            }
         }
         .padding()
     }
 }
 
+struct ExampleView: View {
+    var body: some View {
+        VStack {
+            Text("navigation")
+        }
+    }
+}
+
 #Preview {
     ContentView()
+}
+
+#Preview {
+    ExampleView()
 }
