10b5bfafc1bc6ed681032d85fe0f4aa7b36112db
Crane-tsuru <116482604+Crane-tsuru@users.noreply.github.com>
2024-05-02 17:32:21 +0900

    input view

--- yumemicoding/InputParameterView.swift
+++ yumemicoding/InputParameterView.swift
@@ -21,31 +21,32 @@ struct InputParameterView: View {
                 .font(.title2)
             Spacer()
             VStack(alignment: .leading) {
-                Text("名前")
-                TextField("山田太郎", text: $name)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                .frame(width: 250)
-            }.padding()
-            
-            VStack(alignment: .leading) {
+                HStack {
+                    Text("名前")
+                    TextField("山田太郎", text: $name)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }.padding()
+                
+                Text("誕生日").padding()
                 DatePicker(
-                    "誕生日",
+                    "",
                     selection: $theDate,
                     in: dateRange,
                     displayedComponents: [.date]
                 )
-                    .padding()
-                    .environment(\.locale, Locale(identifier: "ja_JP"))
-            }.padding()
-            
-            HStack {
-                Text("血液型")
-                Picker(selection: $bloodSelection, label: Text("血液型")) {
-                    Text("A").tag(0)
-                    Text("AB").tag(1)
-                    Text("B").tag(2)
-                    Text("O").tag(3)
-                }
+                .datePickerStyle(.wheel)
+                .environment(\.locale, Locale(identifier: "ja_JP"))
+                
+                
+                HStack {
+                    Text("血液型")
+                    Picker("血液型" ,selection: $bloodSelection) {
+                        Text("A").tag(0)
+                        Text("AB").tag(1)
+                        Text("B").tag(2)
+                        Text("O").tag(3)
+                    }
+                }.padding()
             }.padding()
         }
     }
